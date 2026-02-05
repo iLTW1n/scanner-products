7c80d8f8fd7b222c8e35df0f504c7c27beae6180
iLTW1n <jose.osk01@gmail.com>
2026-02-05 11:57:43 -0500

    feat(g): initial project structure

---
+++ src/layouts/root/Root.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/organisms/header';
+import { ReactNode } from 'react';
+
+type RootProps = {
+  children: ReactNode;
+};
+
+export const Root = ({ children }: RootProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
